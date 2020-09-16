92a89c0b1be68ebc3e56afc9d730de12c3f570e9
Brian <b.riverah@uniandes.edu.co>
2020-09-15 21:06:58 -0500

    Se agrego el requerimento 1 en Model.

--- App/model.py
+++ App/model.py
@@ -20,7 +20,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  """
 import config
-import controller as ctr
 import csv
 from DISClib.ADT import list as lt
 from DISClib.ADT import map as mp
@@ -43,16 +42,16 @@ def newCatalog():
 
     catalog = {"movies":None,'ids': None,"productora":None
                 }
-    catalog["movies"]=lt.newList("ARRAY_LIST")
+    catalog["movies"]=lt.newList("ARRAY_LIST",compareRecordIds)
 
-    catalog['ids'] = mp.newMap(30,
-                                   maptype='PROBING',
-                                   loadfactor=0.5,
-                                   comparefunction=compareMapMoviesIds)
-    catalog["productora"]= mp.newMap(30,
-                                   maptype='PROBING',
-                                   loadfactor=0.5,
-                                   comparefunction=1)
+    catalog['ids'] = mp.newMap(400000,
+                                   maptype='CHAINING',
+                                   loadfactor=1,
+                                   comparefunction=moviesIds)
+    catalog["productora"]= mp.newMap(2000,
+                                   maptype='CHAINING',
+                                   loadfactor=1,
+                                   comparefunction=compare_companies_byname)
 
     
     return catalog
@@ -62,14 +61,39 @@ def newCatalog():
 # Funciones para agregar informacion al catalogo
 
 def addmovie(catalog, movie):
-    mp.put(catalog['ids'], int(movie['id']), movie)
-    catalog["movies"]
+    mp.put(catalog['ids'], int(movie["\ufeffid"]), movie)
+
+def addmovie_company(catalogo,nombre_compañia,pelicula):
+    companies=catalogo["productora"]
+    existe_compañia=mp.contains(companies,nombre_compañia)
+    if(existe_compañia):
+        entry = mp.get(companies, nombre_compañia)
+        companie= me.getValue(entry)
+    else:
+        companie = newCompanie(nombre_compañia)
+        mp.put(companies, nombre_compañia, companie)
+    lt.addLast(companie['pelicula'], pelicula)
+
+    cmpavg = companie['vote_average']
+    movieavg = pelicula['vote_average']
+    if (movieavg == 0.0):
+        companie['vote_average'] = float(movieavg)
+    else:
+        companie['vote_average'] = (cmpavg + float(movieavg)) / 2
+
+def newCompanie(name):
+    pelicula = {'name': "", "pelicula": None,  "vote_average": 0}
+    pelicula['name'] = name
+    pelicula['pelicula'] = lt.newList('SINGLE_LINKED', compare_companies_byname)
+    return pelicula
 
 
 # ==============================
 # Funciones de consulta
 # ==============================
-
+def encontrar_compañia(compania,catalogo):
+    companie=mp.get(catalogo["productora"],compania)
+    return companie
 def obtener_primera_pelicula(catalog):
     return mp.get(catalog["ids"],2)
 def obtener_ultima_pelicula(catalog):
@@ -80,7 +104,7 @@ def datos_pelicula(obtener_primera_pelicula,obtener_ultima_pelicula):
     fecha_estreno=obtener_primera_pelicula["value"]["release_date"]
     promedio_votacion=obtener_primera_pelicula["value"]["vote_average"]
     idioma=obtener_primera_pelicula["value"]["spoken_languages"]
-    
+
     titulo2=obtener_ultima_pelicula["value"]["title"]
     fecha_estreno2=obtener_ultima_pelicula["value"]["release_date"]
     promedio_votacion2=obtener_ultima_pelicula["value"]["vote_average"]
@@ -89,59 +113,36 @@ def datos_pelicula(obtener_primera_pelicula,obtener_ultima_pelicula):
 
 
 # ==============================
-# Funciones de Comparacion
+# Funciones de Comparacion \ufeff
 # ==============================
 
 def compareRecordIds(recordA, recordB):
-    if int(recordA['id']) == int(recordB['id']):
+    if int(recordA["\ufeffid"]) == int(recordB["\ufeffid"]):
         return 0
-    elif int(recordA['id']) > int(recordB['id']):
+    elif int(recordA["\ufeffid"]) > int(recordB["\ufeffid"]):
         return 1
     return -1
-    
-def compareMapMoviesIds(id, entry):
+def moviesIds(id1, id2):
     """
-    Compara dos ids de películas, id es un identificador
-    y entry una pareja llave-valor
+    Compara dos ids de libros
     """
-    identry = me.getKey(entry)
-    if (int(id) == int(identry)):
+    id2=int(id2["value"]["\ufeffid"])
+    if (id1 == id2):
         return 0
-    elif (int(id) > int(identry)):
+    elif id1 > id2:
         return 1
     else:
         return -1
-def compareMapMoviesCompany(id=5,entry={"key":3,"value":"hola"}):
+
+def compare_companies_byname(keyname, company):
     """
-    Compara dos ids de películas, id es un identificador
-    y entry una pareja llave-valor
+    Compara dos nombres de autor. El primero es una cadena
+    y el segundo un entry de un map
     """
-    identry = me.getKey(entry)
-    print(identry)
-
-#compareMapMoviesCompany()
-
-#_________________________________________________________________________
-
-def loadCSVFile (file,catalog):
-    dialect = csv.excel()
-    dialect.delimiter=";"
-    with open( config.data_dir + file, encoding="utf-8") as csvfile:
-        row = csv.DictReader(csvfile, dialect=dialect)
-        for elemento in row: 
-            addmovie(catalog,elemento)
-    return catalog
-
-def load_productora_first(file,catalogo,xd):
-    dialect = csv.excel()
-    dialect.delimiter=";"
-    with open( config.data_dir + file, encoding="utf-8") as csvfile:
-        row = csv.DictReader(csvfile, dialect=dialect)
-        if(xd == 0):
-            for elemento in row:
-                if():
-
-        
-vacio=newCatalog()
-catalogox= loadCSVFile("SmallMoviesDetailsCleaned.csv",vacio)
-xdasd=load_productora_first("SmallMoviesDetailsCleaned.csv",vacio,0)
+    authentry = me.getKey(company)
+    if (keyname == authentry):
+        return 0
+    elif (keyname > authentry):
+        return 1
+    else:
+        return -1
